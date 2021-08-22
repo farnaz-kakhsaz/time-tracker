291372509d3e47e23cdc4495b0b488eb82753717
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-22 23:12:41 +0430

    add CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import LandingPage from "./components/pages/landing-page.component";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <CssBaseline />
+      <LandingPage />
+    </>
+  );
 }
 
 export default App;
