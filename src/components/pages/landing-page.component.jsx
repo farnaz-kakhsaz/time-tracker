b67f3552df7652b3b5b808e2aceed76d6aae7c58
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-22 23:20:13 +0430

    add useReducer to ADD_TASK and DELETE_TASK

--- src/components/pages/landing-page.component.jsx
+++ src/components/pages/landing-page.component.jsx
@@ -1,26 +1,93 @@
+import { useState, useReducer } from "react";
 // Components
 import ContainerBase from "../items/container-base/container-base";
-import TextField from "../items/text-field-base/text-field-base";
-import ButtonBase from "../items/button-base/button-base";
+import TextFieldBase from "../items/text-field-base/text-field-base";
 import TypographyBase from "../items/typography-base/typography-base";
+import DialogBase from "../items/dialog-base/dialog-base";
+import BoxBase from "../items/box-base/box-base";
 // Styles
 import { useStyles } from "./landing-page.styles";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      return { ...state, tasksList: [...state.tasksList, action.task] };
+    case "DELETE_TASK":
+      return {
+        ...state,
+        tasksList: state.tasksList.filter((task) => task.id !== action.task.id),
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = { title: "", description: "" };
+
 export default function LandingPage() {
   const classes = useStyles();
+  const [state, setState] = useState({ initialState });
+  const [{ tasksList }, dispatch] = useReducer(reducer, { tasksList: [] });
+
+  const handleInputChange = (event) =>
+    setState((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+
+  const handleAddBtnClick = (title, description) => (event) => {
+    if (title) {
+      dispatch({
+        type: "ADD_TASK",
+        task: {
+          id: new Date().getTime(),
+          title: title,
+          description: description,
+        },
+      });
+    }
+    setState({ ...initialState });
+  };
+
+  const handleDeleteBtnClick = (task) => (event) =>
+    dispatch({ type: "DELETE_TASK", task: task });
+
   return (
     <ContainerBase>
-      <TypographyBase
-        className={classes.typography}
-        variant="h2"
-        component="h1"
-      >
-        Time Tracher
-      </TypographyBase>
-      <div className={classes.textFieldAndBtnParent}>
-        <TextField label="Enter Task Name" />
-        <ButtonBase color="primary">submit</ButtonBase>
-      </div>
+      <BoxBase textAlign="center">
+        <TypographyBase
+          className={classes.typography}
+          variant="h2"
+          component="h1"
+        >
+          Time Tracher
+        </TypographyBase>
+        <DialogBase
+          title="Add Task"
+          handleAddBtnClick={handleAddBtnClick(state.title, state.description)}
+          titleValue={state.title}
+        >
+          <TextFieldBase
+            value={state.title}
+            onChange={handleInputChange}
+            label="Title"
+            name="title"
+          />
+          <TextFieldBase
+            value={state.description}
+            onChange={handleInputChange}
+            label="Description"
+            name="description"
+          />
+        </DialogBase>
+        {tasksList?.map((item, index) => (
+          <div key={index} onClick={handleDeleteBtnClick(item)}>
+            {item.id}
+            {item.title}
+            {item.description}
+          </div>
+        ))}
+      </BoxBase>
     </ContainerBase>
   );
 }
--- src/components/pages/landing-page.styles.js
+++ src/components/pages/landing-page.styles.js
@@ -3,20 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 export const useStyles = makeStyles((theme) => ({
   typography: {
     textAlign: "center",
-  },
-  textFieldAndBtnParent: {
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      width: "100%",
-      height: 105,
-    },
-    [theme.breakpoints.up("sm")]: {
-      flexDirection: "row",
-      width: 305,
-      height: "100%",
-      margin: "0 auto",
-    },
-    display: "flex",
-    justifyContent: "space-between",
+    margin: theme.spacing(10, 0, 5),
   },
 }));
