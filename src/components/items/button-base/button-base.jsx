e4ad93cee5a7f9b45ebb5c5441b06d53340c06d1
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-23 16:30:29 +0430

    make variant in ButtonBase dynamically

--- src/components/items/button-base/button-base.jsx
+++ src/components/items/button-base/button-base.jsx
@@ -1,9 +1,9 @@
 import Button from "@material-ui/core/Button";
 import PropTypes from "prop-types";
 
-export default function ButtonBase({ children, color, ...rest }) {
+export default function ButtonBase({ children, variant, color, ...rest }) {
   return (
-    <Button variant="contained" color={color} {...rest}>
+    <Button variant={variant} color={color} {...rest}>
       {children}
     </Button>
   );
@@ -11,6 +11,7 @@ export default function ButtonBase({ children, color, ...rest }) {
 
 ButtonBase.propTypes = {
   children: PropTypes.node.isRequired,
+  variant: PropTypes.string,
   color: PropTypes.string,
   rest: PropTypes.any,
 };
