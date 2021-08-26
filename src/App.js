5356a8659af4b59aaa0cf8ccc5f76c6594c095b4
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-08-26 22:54:38 +0430

    add theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LandingPage from "./components/pages/landing-page.component";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { theme } from "./theme/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <LandingPage />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@material-ui/core/styles";
+
+export const theme = createTheme({
+  palette: {
+    tertiary: {
+      light: "#6fbf73",
+      main: "#4caf50",
+      dark: "#357a38",
+      contrastText: "#fff",
+    },
+  },
+});
